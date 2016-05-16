3245714ff5f24692c6c6a6252e6c9fb861c091a3
Carolyn <crupar@gmail.com>
2016-05-16 14:07:42 -0700

    checkpoint-21: working on it

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,22 +1,21 @@
-require_relative 'entry'
 
-class AddressBook
-  attr_reader :entries
+ require_relative 'entry'
+ require "csv"
 
-  def initialize
-    @entries = []
-  end
-
-
-   def add_entry(name, phone_number, email)
-     index = 0
-     entries.each do |entry|
-       if name < entry.name
-         break
-       end
-       index += 1
-     end
-
+ class AddressBook
+   attr_reader :entries
+ def add_entry(name, phone_number, email)
      entries.insert(index, Entry.new(name, phone_number, email))
+
+   end
+
+   def import_from_csv(file_name)
+     csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+      csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
    end
  end
+end
