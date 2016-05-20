7d701df16b34051cf1adab06862d0b7e0eabd51f
Carolyn <crupar@gmail.com>
2016-05-20 12:04:09 -0700

    Checkpoint 22: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -37,6 +37,28 @@ class AddressBook
   end
 
   def binary_search(name)
+    # #1
+    lower = 0
+    upper = entries.length - 1
+
+    # #2
+    while lower <= upper
+      # #3
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+      # #4
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+    # #5
+    return nil
   end
 
 end
